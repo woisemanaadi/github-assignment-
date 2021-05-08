08996b76fa6b629ee4f3bf402c3867d994fa0683
woisemanaadi <81508119+woisemanaadi@users.noreply.github.com>
2021-05-08 12:37:08 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,45 @@
+var fixedRect, movingRect;
+var leftObject;
+var rightObject;
+
+function setup() {
+  createCanvas(1200,800);
+  fixedRect = createSprite(400, 100, 50, 80);
+  fixedRect.shapeColor = "green";
+  fixedRect.debug = true;
+
+  leftObject=createSprite(400,200,50,50);
+  rightObject=createSprite(600,200,50,50);
+  leftObject.velocityX=3;
+  rightObject.velocityX=-3;
+  movingRect = createSprite(400, 800,80,30);
+  movingRect.shapeColor = "green";
+  movingRect.debug = true;
+
+  movingRect.velocityY = -5;
+  fixedRect.velocityY = +5;
+}
+
+function draw() {
+  background(0,0,0);  
+
+  bounceOff(leftObject,rightObject);
+  bounceOff(movingRect,fixedRect);
+  drawSprites();
+
+
+}
+
+function bounceOff(object1,object2){
+  if (object1.x - object2.x < object2.width/2 + object1.width/2
+    && object2.x - object1.x < object2.width/2 + object1.width/2) {
+      object1.velocityX = object1.velocityX * (-1);
+      object2.velocityX = object2.velocityX * (-1);
+}
+if (object1.y - object2.y < object2.height/2 + object1.height/2
+  && object2.y - object1.y < object2.height/2 + object1.height/2){
+    object1.velocityY = object1.velocityY * (-1);
+    object2.velocityY = object2.velocityY * (-1);
+}
+
+}
